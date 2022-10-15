09e8caa6971623f6ef9412c4c572ac835c1d7048
Thomas M <60380026+thomasmk10@users.noreply.github.com>
2022-10-15 04:36:19 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
+import cv2
+
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
@@ -32,13 +34,34 @@ class labels():
 def uploadImage(event=None):
     fileName = filedialog.askopenfilename()
 
+def LiveCamBtn():
+    cam = cv2.VideoCapture(0)
+    while cam.isOpened():
+        ret, frame1 = cam.read()
+        ret, frame2 = cam.read()
+        diff = cv2.absdiff(frame1, frame2)
+        gray = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+        dilated = cv2.dilate(thresh, None, iterations=3)
+        contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+        for c in contours:
+            if cv2.contourArea(c) < 10000:
+                continue
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if cv2.waitKey(10) == ord('0'):
+            break
+        cv2.imshow('Test camera', frame1)
+
 root = tk.Tk()
 root.geometry("1920x1080")
 
 startMsg = labels(root, "Header", "Hello User!")
 startMsg.place(x=620, y=200)
 
-liveBtn = tk.Button(root, text="Use Live View").place(x=650, y=300)
+liveBtn = tk.Button(root, text="Use Live View", command=LiveCamBtn).place(x=650, y=300)
 picBtn = tk.Button(root, text="Submit Image", command=uploadImage).place(x=650, y=400)
 
 root.mainloop()
